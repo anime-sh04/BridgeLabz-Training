1498a6e31b054485c6eaada8ba0dd577a24f7efe
Kur0._. <animeshrajpoot004@gmail.com>
2025-12-17 16:34:39 +0530

    [Animesh Rajpoot] Perimeter Of Rectangle

---
+++ core-java-practice/gcr-codebase/programming-elements/PerimeterOfRectangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int b = sc.nextInt();
+
+        int p = 2*(l+b);
+        System.out.println(p);
+    }
+}
+ {
+    
+}
