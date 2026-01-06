83c9bf821ba4b863438c070d2dc7ae357cef3fc6
Kur0._. <animeshrajpoot004@gmail.com>
2026-01-06 16:37:24 +0530

    [Animesh Rajpoot] Add.Scenario Based Question Project

---
+++ oops-csharp-practice/scenario-based/customer-service-call-log/Program.cs
@@ -0,0 +1,25 @@
+﻿using Service;
+using System;
+
+namespace Service
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CallLogManager manager = new CallLogManager(10);
+
+            manager.AddCallLog("234564321", "Class Cancelled");
+            manager.AddCallLog("2345432", "Call drop");
+            manager.AddCallLog("23454322", "Failed in Subject");
+            manager.AddCallLog("23456543324", "C# Classes");
+
+            manager.SearchByKeyword("Class");
+
+            DateTime from = DateTime.Now.AddMinutes(-5);
+            DateTime to = DateTime.Now;
+
+            manager.FilterByTime(from,to);
+        }
+    }
+}
