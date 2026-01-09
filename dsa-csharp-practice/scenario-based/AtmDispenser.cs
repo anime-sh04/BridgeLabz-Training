cfcaa97a2b99fc96cb844f13af833a6e991d6d99
Kur0._. <animeshrajpoot004@gmail.com>
2026-01-09 14:11:43 +0530

    [Animesh Rajpoot] Refactor.Scenario 3 Atm Dispenser

--- dsa-csharp-practice/scenario-based/AtmDispenser.cs
+++ dsa-csharp-practice/scenario-based/AtmDispenser.cs
@@ -5,18 +5,31 @@
         void DispenseNotes(int amount)
         {
             //int[] notes = { 500, 200, 100, 50, 20, 10, 5, 2, 1 };  // Scenario A with 500 note
-            int[] notes = {200, 100, 50, 20, 10, 5, 2, 1 };  // Scenario B without 500 note
-            int i = 0;
-            while (amount != 0)
+
+            //int[] notes = {200, 100, 50, 20, 10, 5, 2, 1 };  // Scenario B without 500 note
+            //int i = 0;
+            //while (amount != 0)
+            //{
+            //    int count = amount / notes[i];
+            //    if(count > 0)
+            //    {
+            //        Console.WriteLine($"{count} note of {notes[i]} dispensed!");
+            //        amount = amount %notes[i];
+            //    }
+            //    i++;
+            //}
+            int[] notes = { 200, 100, 50 }; // Scenario C - Lets assume we only have these notes
+            int despenseAmount = 0;
+            for(int i=0; i<notes.Length; i++)
             {
                 int count = amount / notes[i];
-                if(count > 0)
+                if (count > 0)
                 {
-                    Console.WriteLine($"{count} note of {notes[i]} dispensed!");
-                    amount = amount %notes[i];
+                    despenseAmount += count * notes[i];
+                    amount -= count * notes[i];
                 }
-                i++;
             }
+            Console.WriteLine($"Sorry! Only {despenseAmount} amount can be withdrawn,we dont have changes");
         }
         static void Main(string[] args)
         {
