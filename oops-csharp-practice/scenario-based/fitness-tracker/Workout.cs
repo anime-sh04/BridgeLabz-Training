3f90c493385d85f56d23d42415fd6c48e71d16ed
Kur0._. <animeshrajpoot004@gmail.com>
2026-01-09 12:11:10 +0530

    [Animesh Rajpoot] Add.Scenario Based Problem

---
+++ oops-csharp-practice/scenario-based/fitness-tracker/Workout.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fitness_Tracker
+{
+    internal class Workout
+    {
+        private string workoutname;
+        private double duration;
+        public string WorkoutName
+        {
+            get { return workoutname; } 
+            set { workoutname = value; }
+        }
+        public double Duration
+        {
+            get { return duration; }
+            set { duration = value; }
+        }
+        public Workout(string workoutname, double duration)
+        {
+            this.workoutname = workoutname;
+            this.duration = duration;
+        }
+        public override string ToString()
+        {
+            return $"Workout Name : {workoutname}\nDuration : {duration}";
+        }
+
+    }
+}
