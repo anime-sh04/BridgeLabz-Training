1fe2afef0664b0b29ce0ea3ffe9a4e2971028941
Kur0._. <animeshrajpoot004@gmail.com>
2026-01-09 12:38:06 +0530

    [Animesh Rajpoot] Add.Scenario A of Atm Dispenser

---
+++ dsa-csharp-practice/scenario-based/AtmDispenser.cs
@@ -0,0 +1,33 @@
+﻿namespace ATM
+{
+    internal class AtmDispenser
+    {
+        void DispenseNotes(int amount)
+        {
+            int[] notes = { 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+            int i = 0;
+            while (amount != 0)
+            {
+                int count = amount / notes[i];
+                if(count > 0)
+                {
+                    Console.WriteLine($"{count} note of {notes[i]} dispensed!");
+                    amount = amount %notes[i];
+                }
+                i++;
+            }
+        }
+        static void Main(string[] args)
+        {
+			Console.WriteLine("Enter the amount you want to withdraw :");
+            int amount = int.Parse(Console.ReadLine());
+            if (amount == 0) 
+            { 
+                Console.WriteLine("Amount cant be 0"); 
+                return; 
+            }
+            AtmDispenser atm = new AtmDispenser();
+            atm.DispenseNotes(amount);
+        }
+    }
+}
