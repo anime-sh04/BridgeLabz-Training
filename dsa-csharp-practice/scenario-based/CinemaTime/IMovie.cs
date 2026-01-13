dad12cd40b0ca09449e817b154a42f9158efc0a7
Kur0._. <animeshrajpoot004@gmail.com>
2026-01-13 14:39:52 +0530

    [Animesh Rajpoot] Add.Scenario Based Question

---
+++ dsa-csharp-practice/scenario-based/CinemaTime/IMovie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie
+{
+    internal interface IMovie
+    {
+        Movie AddMovie();
+        void SearchMovie();
+        void DisplayMovies();
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/CinemaTime/MovieUtilites.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Movie
+{
+    internal class MovieUtilites : IMovie
+    {
+        private Movie movie;
+        Movie[] movies = new Movie[10];
+        private int movieCount = 0;
+
+        public Movie AddMovie()
+        {
+            if (movieCount >= movies.Length) { Console.WriteLine("Movie list is full"); return null; }
+
+            Movie movie = new Movie();
+
+            Console.Write("Enter title: ");
+            movie.Title = Console.ReadLine();
+
+            Console.Write("Enter hours: ");
+            int hr = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter minutes: ");
+            int min = int.Parse(Console.ReadLine());
+
+            movie.Time = new DateTime(1,1,1,hr,min,0);
+
+            movies[movieCount] = movie;
+            movieCount++;
+
+            Console.WriteLine("Movie added successfully!");
+            return movie;
+
+
+        }
+        public void SearchMovie()
+        {
+            Console.Write("Enter keyword: ");
+            string keyword = Console.ReadLine();
+
+            for (int i = 0; i < movieCount; i++)
+            {
+                if (movies[i].Title.Contains(keyword, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Found Movie:\n" + movies[i]);
+                    return;
+                }
+            }
+
+            Console.WriteLine("Movie Not Found");
+        }
+        public void DisplayMovies()
+        {
+            if (movieCount == 0)
+            {
+                Console.WriteLine("No movies to display");
+                return;
+            }
+
+            for (int i = 0; i < movieCount; i++)
+            {
+                Console.WriteLine(movies[i] + "\n");
+            }
+        }
+    }
+}
