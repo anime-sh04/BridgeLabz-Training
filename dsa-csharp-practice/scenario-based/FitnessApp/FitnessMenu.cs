a722c58611d5e918be63b5c71b6ab1107fb83f38
Kur0._. <animeshrajpoot004@gmail.com>
2026-01-17 16:06:50 +0530

    [Animesh Rajpoot] Add.Scenario Based Question

---
+++ dsa-csharp-practice/scenario-based/FitnessApp/FitnessMenu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp
+{
+    internal class FitnessMenu
+    {
+        IFitness users = new FitnessUtilityImpl();
+        public void Menu()
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Add User");
+                Console.WriteLine("2. Display LeaderBoard");
+                Console.WriteLine("3. Display Users");
+                Console.WriteLine("0. Exit");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch(choice)
+                {
+                    case 0:
+                        return;
+                    case 1:users.AddUser();break;
+                    case 2:users.DisplayWithBubbleSort();break;
+                    case 3:users.Display();break;
+                    default:Console.WriteLine("INvalid Choice");break;
+                }
+            }
+        }   
+    }
+}
---
+++ dsa-csharp-practice/scenario-based/FitnessApp/FitnessUtilityImpl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp
+{
+    internal class FitnessUtilityImpl : IFitness
+    {
+        User[] users = new User[20];
+        int count = 0;
+
+        public void AddUser()
+        {
+            Console.WriteLine("Enter Name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter number of steps : ");
+            int steps = int.Parse(Console.ReadLine());
+            users[count] = new User(name, steps);
+            count++;
+        }
+
+        public void DisplayWithBubbleSort()
+        {
+            for (int i = 0; i < count - 1; i++)
+            {
+                for (int j = 0; j < count - i - 1; j++)
+                {
+                    if (users[j].Steps > users[j + 1].Steps)
+                    {
+                        int temp = users[j].Steps;
+                        users[j].Steps = users[j + 1].Steps;
+                        users[j + 1].Steps = temp;
+
+
+                        string tempName = users[j].Name;
+                        users[j].Name = users[j + 1].Name;
+                        users[j + 1].Name = tempName;
+                    }
+                }
+            }
+
+            Display();
+        }
+
+        public void Display()
+        {
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"{i + 1} {users[i].Name} with {users[i].Steps}\n");
+            }
+        }
+    }
+}
